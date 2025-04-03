c6de1ccadd3eb8d30d2fc084e2c4fc2c92816576
RacquelNinaDennison <racquelnina@gmail.com>
2025-04-03 19:06:03 +0200

    Updated with new results

--- testing/knowledge_bases_iclp/ranked_knowledgebases/query_tests/extract_ranked_tests.py
+++ testing/knowledge_bases_iclp/ranked_knowledgebases/query_tests/extract_ranked_tests.py
@@ -1,7 +1,7 @@
 import json 
 
 prefix = 'ranked_query'
-knowledge_base_count = [25,50,100,150]
+knowledge_base_count = [25,50,75,125,100,150,175,200]
 
 
 for knowledge_base in knowledge_base_count:
@@ -10,7 +10,7 @@ for knowledge_base in knowledge_base_count:
         with open(filename) as file:
             jsonConverted = json.load(file)
         timeTaken = jsonConverted["Time"]["Total"]
-        with open(f"results/{prefix}_{knowledge_base}","+a") as written_results:
+        with open(f"results/{prefix}_{knowledge_base}.txt","+a") as written_results:
             written_results.writelines(f"{timeTaken}\n")
 
     
